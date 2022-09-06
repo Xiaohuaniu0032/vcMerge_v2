b2cb8b840f4818b378d16a251b0d16c5f67e6f1e
longfei.fu <longfei.fu@thermofisher.com>
2022-09-06 09:58:41 +0800

    first commit

---
+++ vcMerge_pipeline_v2.pl
@@ -0,0 +1,110 @@
+use strict;
+use warnings;
+use File::Basename;
+use Getopt::Long;
+use FindBin qw/$Bin/;
+
+my ($report_dir,$outdir) = @ARGV;
+
+
+# TVC插件目录
+my $vc_dirs_aref = &get_vc_dir_by_run_time($report_dir);
+
+# 循环每个目录
+for my $dir (@{$vc_dirs_aref}){
+	my $vc_dir = "$report_dir/plugin_out/$dir";
+	my @vcf = glob "$vc_dir/*/alleles.xls";
+
+	my %sample_vars; # 记录特定样本的变异
+	my %all_vars;    # 记录这个run下所有样本的变异
+	my %sample;      # 样本名称(barcode编号)
+
+	for my $vcf (@vcf){
+		my $base_dir = dirname($vcf);
+		my $barcode = basename($base_dir); # IonXpress_001
+		$sample{$barcode} = 1;
+
+		open VCF, "$vcf" or die;
+		<VCF>; # skip header line
+		while (<VCF>){
+			chomp;
+			next if /^$/;
+			my @arr = split /\t/;
+
+			my $chr = $arr[0];
+			my $pos = $arr[1];
+			my $ref = $arr[2];
+			my $alt = $arr[3];
+			my $gt = $arr[4]; # Homozygous / Heterozygous
+			my $freq = $arr[6];
+			my $qual = $arr[7];
+			my $ori_cov = $arr[18]; # Original Coverage
+
+			next if ($arr[4] ne "Homozygous" and $arr[4] ne "Heterozygous");
+			
+			my $var = "$arr[0]\:$arr[1]\:$arr[2]\:$arr[3]"; # 2019-nCoV:210:G:T
+			$sample_vars{$barcode}{$var} = "$freq\|$ori_cov";
+			push @{$all_vars{$pos}}, $var; # 1) one pos may has diff variants; 2) may contain dup vars
+		}
+		close VCF;
+	}
+			
+
+	my $outfile = "$outdir/$dir\.TSVC_variants.merged.vcf.xls";
+	print "[Merge VCF is]: $outfile\n";
+	
+	open O, ">$outfile" or die;
+	print O "Chrom\tPosition\tRef\tVariant";
+
+	my @barcode = keys %sample;
+	my @barcode_sort = sort {$a cmp $b} @barcode;
+
+	for my $s (@barcode_sort){
+		print O "\t$s";
+	}
+	print O "\n";
+
+	# sort variant by pos
+	foreach my $pos (sort { $a <=> $b } keys %all_vars){
+		my $var_aref = $all_vars{$pos}; # one pos may have multi var type
+		my %uniq_var;
+		for my $var (@{$var_aref}){
+			$uniq_var{$var} = 1;
+		}
+
+		for my $var (keys %uniq_var){ # 2019-nCoV:210:G:T
+			my @var = split /\:/, $var;
+			print O "$var[0]\t$var[1]\t$var[2]\t$var[3]";
+			# 按样本顺序输出频率和深度信息
+			for my $s (@barcode_sort){
+				my $freq;
+				if (exists $sample_vars{$s}{$var}){
+					$freq = $sample_vars{$s}{$var};
+				}else{
+					$freq = "NA";
+				}
+				print O "\t$freq";
+			}
+			print O "\n";
+		}
+	}
+	close O;
+}
+
+
+
+sub get_vc_dir_by_run_time{
+	my ($dir) = @_; # /results/analysis/output/Home/GH002-20070043-88-guocan_S5-20220817-chip1_531
+	# variantCaller插件可能运行多次,得到这个run所有的tvc插件结果
+	my @startplugin_json = glob "$dir/plugin_out/*/startplugin.json"; # /results/analysis/output/Home/2019-nCoV-map2hg19-exon-virus_241/plugin_out/variantCaller_out.1257/startplugin.json
+	my @vc_dirs;
+	for my $json (@startplugin_json){
+		my $basedir = dirname($json); # /results/analysis/output/Home/GH002-20070043-88-guocan_S5-20220817-chip1_531/plugin_out/variantCaller58_out.1955
+		my $vc_name = basename($basedir); # variantCaller58_out.1955
+		if ($vc_name =~ /^variantCaller_out/){
+			push @vc_dirs, $vc_name;
+		}
+	}
+
+	return(\@vc_dirs);
+}
---
+++ vcMerge_v2.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2019 Thermo Fisher Scientific. All Rights Reserved
+# Author: Haktan Suren
+
+# This has been tested only on S5XL, S5 Prime with 5.10 and 5.12. 
+
+import common
+import glob
+import sys
+import subprocess
+import json
+import os
+import re
+import shutil
+from ion.plugin import *
+
+# 2022-9-5
+
+class vcMerge_v2(IonPlugin):
+  version = '1.0.0.0'
+  envDict = dict(os.environ)
+  author = "longfei.fu@thermofisher.com"
+  runtypes = [RunType.FULLCHIP, RunType.THUMB, RunType.COMPOSITE]
+
+  def merge(self):
+    report_dir = self.envDict['ANALYSIS_DIR'] # /results/analysis/output/Home/Auto_xxx
+    outdir = '/results/analysis' + self.envDict['TSP_URLPATH_PLUGIN_DIR'] # /results/analysis/output/Home/Auto_xxx/plugin_out/variantCallerMerge.x
+    this_dir = self.envDict['DIRNAME']
+    cmd = "perl %s/vcMerge_pipeline_v2.pl %s %s" % (this_dir,report_dir,outdir)
+
+    print "report dir is: %s" % (report_dir)
+    print "plugin outdir is: %s" % (outdir)
+    print "cmd is: %s" % (cmd)
+
+    os.system(cmd)
+    print "Finished the variantCallerMerge plugin."
+
+  def launch(self,data=None):
+    print "Start running the variantCallerMerge plugin."
+
+    start_plugin_json = '/results/analysis' + os.path.join(os.getenv('TSP_URLPATH_PLUGIN_DIR'),'startplugin.json')
+    print(start_plugin_json)
+    with open(start_plugin_json, 'r') as fh:
+      spj = json.load(fh)
+      net_location = spj['runinfo']['net_location']
+
+    
+    report_dir = self.envDict['ANALYSIS_DIR'] # /results/analysis/output/Home/Auto_xxx
+    report_number = report_dir.split('_')[-1]
+    this_plugin_dir = os.getenv('TSP_URLPATH_PLUGIN_DIR').split('/')[-1]
+    url_path = os.getenv('TSP_URLPATH_PLUGIN_DIR')
+    
+    self.merge()
+    with open("variantCallerMerge_block.html","w") as f:
+      f.write('<html><body>To download: "Right Click" -> "Save Link As..."<br>\n')
+      for merge_vcf in glob.glob('*.merged.vcf.xls'):
+        print(merge_vcf)
+        f.write('<a href="%s">%s</a><br>\n'
+                % (os.path.join(net_location,url_path,merge_vcf),merge_vcf))
+        f.write('</body></html>\n')
+    
+    return True
+
+
+if __name__ == "__main__":
+    PluginCLI()
